f4ec3e6cefc836addee5b9137e9a4154c9a05dc3
El0H0Amine <106432432+El0H0Amine@users.noreply.github.com>
2023-01-23 18:43:54 +0100

    Add func

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,31 @@ import { Input } from './components/input/input';
 import { Label } from './components/label/label';
 import { SubmitButton } from './components/submit-button/submit-button';
 import { FormRow } from './components/form-row/form-row';
+import { RiSendPlaneFill } from 'react-icons/ri';
+import SubmitButton_module from './components/submit-button/submit-button.module.scss';
 
 function App() {
+    function handleOnSubmit(event: React.SyntheticEvent<HTMLFormElement>){
+        event.preventDefault();
+        const formData: Record<string, string> = {};
+        function isInputNamedElement(e: Element): e is HTMLInputElement & { name: string } {
+            return 'value' in e && 'name' in e;
+        }
+        Array.from(event.currentTarget.elements).filter(isInputNamedElement).forEach(elm => {
+            if (!elm.name) return;
+            formData[elm.name] = elm.value;
+        })
+        console.log('Data', formData);
+    }
+
     return (
         <div className={styles.App}>
             <h1 className={styles.title}>Contact Us</h1>
-            <p className={styles.description}>
+            <p className={styles.description}>  
                 Have a question ? Fill out this form and we will answer you as soon as possible.
             </p>
             <p className={styles.note}>All fields are required</p>
-            <form className={styles.form}>
+            <form  onSubmit={handleOnSubmit} className={styles.form}>
                 <FormRow>
                     <Label htmlFor={'name'}>Name</Label>
                     <Input id={'name'} name={'name'}></Input>
@@ -26,7 +41,10 @@ function App() {
                     <Input id={'message'} name={'message'}></Input>
                 </FormRow>
                 <FormRow>
-                    <SubmitButton>Submit</SubmitButton>
+                    <SubmitButton>
+                        <RiSendPlaneFill />
+                        Submit
+                    </SubmitButton>
                 </FormRow>
             </form>
         </div>
